7476c0012644af7250e12e146efe86f41d937f7a
Rafael Jegundo <rafael.jegundo@gmail.com>
2011-04-12 17:18:31 +0100

    minor

--- data.py
+++ data.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
-## Tratamento da msg
-
 from string import split
-from fcs import calc_crc
+from fcs import calc_crc, conector, destuffed, crc_test
 
-# data is the captured message
+"""
+data is the captured message. Example: {7E}{42}{7D}{5E}{00}{0B}{7D}{5D}{25}{00}{00}{A4}{13}{00}{00}{33}{85}{86}
+{00}{00}{98}{01}{84}{06}{4C}{18}{AF}{C7}{98}{07}{9F}{AE}{C8}{C3}{51}{71}{FD}{44}
+{E2}{FF}{00}{00}{C1}{01}{C3}{01}{2B}{F8}{7E}
+"""
 
 data = "{7E}{42}{7D}{5E}{00}{0B}{7D}{5D}{25}{00}{00}{A4}{13}{00}{00}{33}{85}{86}\
 {00}{00}{98}{01}{84}{06}{4C}{18}{AF}{C7}{98}{07}{9F}{AE}{C8}{C3}{51}{71}{FD}{44}\
 {E2}{FF}{00}{00}{C1}{01}{C3}{01}{2B}{F8}{7E}"
+
+# Converting the string to a more treatable format
 
 data = split(data[1:len(data)-1],'}{')
 
@@ -17,7 +21,8 @@ data = map (lambda x: '0x' + x, data)
 
 data = map (eval, data)
 
-## Verificação do 7E inicial e final
+
+## Verifing initial and final 0x7E
 
 if data[0] == 0x7E:
 	pass
@@ -33,53 +38,28 @@ for a in data[1:-1]:
 	if a == 0x7E:
 		print "To many 0x7E in the message"
 
-## Destuffing
+## Validating CRC
 
-a = 0
+print crc_test(data)
 
-while a < len(data):
-	if hex(data[a]) == hex(0x7D):
-		if hex(data[a+1]) == hex(0x5E):
-			data[a] = 0x7E
-			data.pop(a+1)
-			a += 1
-		if hex(data[a+1]) == hex(0x5D):
-			data[a] = 0x7D
-			data.pop(a+1)
-			a += 1
-	else:
-		a += 1
-					
-## Confimar CRC
+# Removing Start and Stop Bit
 
 data = data[1:-1]
 
-de_stuff = data[:]
 
-print map(hex,data)
+## Destuffing
 
-crc = 0x0000;
-
-for b in data[:-2]:
-    crc = calc_crc(crc, b);
-
-print "CRC calculado:", hex(crc)
-
-crc_esperado = data[-1]*256 + data[-2]
-
-print "CRC recebido:", hex(crc_esperado)
-
+data = destuffed(data)
 
 length = data[5]
 
-print 'Length is ', hex(length), 'aka', length
+print 'Length is', length
 
-xmesh_msg = de_stuff[6:-2]
+## Filtering content by removing the tinyOS Header
 
-def conector(a):
-        return a[1]*256 + a[0]
+xmesh_msg = data[6:-2]
 
-## Esmiúçar o payload
+## Printing out the payload -> Convert to dictionary
 
 print 'Multihop Header'
 print 'sourceAddr', hex(xmesh_msg[0]), hex(xmesh_msg[1])
@@ -90,7 +70,7 @@ print 'Sensor Header'
 print 'Sensor Board ID', xmesh_msg[7] 
 print 'Sensor Packet ID', xmesh_msg[8]
 print 'Parent', conector(xmesh_msg[9:11])
-print 'Payload'
+print 'Data Payload'
 print 'Voltage', conector(xmesh_msg[11:13]) 
 print 'humidade', conector(xmesh_msg[13:15])
 print 'humtemp', conector(xmesh_msg[15:17])
@@ -105,16 +85,12 @@ print 'taosch1', conector(xmesh_msg[31:33])
 print 'ax', conector(xmesh_msg[33:35])
 print 'ay', conector(xmesh_msg[35:37])
 
+## Converting payload data to real-world units
 
-## Todos os valores do conector(xmesh_msg[x:x] ainda estao afectados pelo facto
-## de o programa nao estar a converter os 7D5D em 7D e os 7D5E em 7E logo ao
-## tentarem correr o programa dará um erro até ser corrigido o problema que foi
-## referido. Se quiserem experimentar o programa alterem todos os parâmetros
-## do xmesh para dois valores acima
-## Calculo dos parametros do payload
-print 'Calculo dos parametros do payload'
+print 'Converting payload data to real-world units'
 
-## CALCULO DA BATERIA
+## Voltage
+
 valortensaomedido=conector(xmesh_msg[11:13]) 
 BV_calc=0
 BV_calc= 1252.352 / valortensaomedido
@@ -125,10 +101,10 @@ humid_medida= conector(xmesh_msg[13:15])
 humtemp_medida = conector(xmesh_msg[15:17])
 print humtemp_medida
 humtemp_calc = -38.4 + (0.0098*humtemp_medida)
-print 'O valor da temperatura e', humtemp_calc,'ºC'
+print 'O valor da temperatura e', humtemp_calc,'C'
 
 
-print ustr(humid_medida)
+print humid_medida
 humid_calc=(0.0098 * humtemp_medida - 63.4)*(0.01+0.00008*humid_medida)-4 + (0.0405*humid_medida) - (0.0000028*humid_medida*humid_medida)
 print 'O valor da humidade e', humid_calc,'%'
 
@@ -152,7 +128,7 @@ pressao_medido=conector(xmesh_msg[27:29])
 UTI= (8 * C5) + 20224
 dT= prtemp_medido - UTI
 prtemp_calc= (200 + (dT * (C6+50)/float(1024)))/float(10)
-print ' O valor do prtemp e', prtemp_calc,'ºC'
+print ' O valor do prtemp e', prtemp_calc,'C'
 OFF= (C2*4 +((C4-512)*dT))/float(4096)
 SENS= (C1 + (C3*dT)/float(1024))+ 24576
 X=((SENS*(pressao_medido-7168))/float(4096)) - OFF
